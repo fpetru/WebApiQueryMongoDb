cb4a2f3ecd4c5b20d923bfe1c6e972f6d7b7fdfb
Petru Faurescu <petru@qappdesign.com>
2017-10-31 22:18:15 +0100

    update dataset

--- Data/createIndexAndProcessData.js
+++ Data/createIndexAndProcessData.js
@@ -2,17 +2,17 @@ db = db.getSiblingDB('TravelDb');
 
 // add indexes
 db.WikiVoyage.createIndex( { City: 1, Action: 1 } );
-// db.Countries.createIndex( { "country-name": 1 }, { unique: true } );
-db.Countries.createIndex( { "alpha-2": 1 }, { unique: true } );
+db.Countries.createIndex( { "Name": 1 }, { unique: true } );
+db.Countries.createIndex( { "Alpha2": 1 }, { unique: true } );
 
-// create a more rich collection for cities
+// create a richer collection for cities
 db.Cities.aggregate([
     {
         // join with Countries collection
-        $lookup: {from: "Countries", localField: "CountryCode", foreignField: "alpha-2", as: "Matched"}
+        $lookup: {from: "Countries", localField: "CountryCode", foreignField: "Alpha2", as: "Matched"}
     },
     {
-      // fields you want to fetch 
+      // fields to fetch  
       $project: {
         Name: "$AsciiName",  
         AlternateNames: {$split: ["$AlternateNames", ","]},
@@ -24,10 +24,24 @@ db.Cities.aggregate([
         TimeZone: "$Timezone"
       }
     },
-   { 
-       $out : "CitiesNew" 
-   }      
+    {
+      // final fields to retrieve (including also the country fields) 
+      $project: {
+        Name: 1,  
+        AlternateNames: 1,
+        Latitude: 1,
+        Longitude: 1,
+        Population: 1,
+        DigitalElevationModel: 1,
+        CountryName: "$Country.Name",
+        CountryCode: "$Country.Alpha2",     
+        Region: "$Country.Region", 
+        Subregion: "$Country.Subregion", 
+        TimeZone: 1
+      }
+    },
+    { 
+        $out : "CitiesNew" 
+    }     
 ]);
-
-
 
